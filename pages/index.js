ac8256b1e4c43df73a6075e81016641b33f43a35
Eduardo Naoto Saito <eduardo.saito@intra.trizy.com.br>
2021-01-25 20:57:46 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,50 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+export const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  height: 100vh;
+  background-position: center;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      <Container>
+        <Widget>
+          <Widget.Header>
+              <h1>FAKE NEWS</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Você é capaz de identificar uma fake news? Coloque seu nome abaixo, clique em jogar e descubra quantas notícias você pode desmascarar!</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Redes Sociais</h1>
+            <p>Instagram</p>
+            <p>LinkedIn</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </Container>
+      <GitHubCorner projectUrl="https://github.com/edusaitto"/>
+    </BackgroundImage>
+  );
 }
